ee13523db34bf40ac266a1a316733ab74f1aeacf
Nitemice <webmaster@nitemice.net>
2021-04-18 16:40:18 +1000

    Updated common functions
    Added comments + new version of stripXml function

--- common.js
+++ common.js
@@ -1,11 +1,16 @@
+// Common GAS Functions
+// v2.0.0 - 2021-04-18
+
 var common = {
 
+    // Retrieve file as JSON object
     grabJson: function(id)
     {
         var file = DriveApp.getFileById(id).getAs("application/json");
         return JSON.parse(file.getDataAsString());
     },
 
+    // Write JSON object as string to file
     saveJson: function(id, content)
     {
         var file = DriveApp.getFileById(id);
@@ -13,6 +18,7 @@ var common = {
         file.setContent(JSON.stringify(content));
     },
 
+    // Get a ref to given folder, or create one if it doesn't exist
     findOrCreateFolder: function(parentDir, foldername)
     {
         // See if there's already a folder in the indicated Google Drive folder
@@ -31,6 +37,7 @@ var common = {
         }
     },
 
+    // Get a ref to given file, or create one if it doesn't exist
     findOrCreateFile: function(parentDir, filename)
     {
         // See if there's already a file in the indicated Google Drive folder
@@ -48,6 +55,7 @@ var common = {
         }
     },
 
+    // Write to file, only if different to existing content
     updateOrCreateFile: function(parentDir, filename, content)
     {
         var file = common.findOrCreateFile(parentDir, filename);
@@ -62,6 +70,7 @@ var common = {
         return file;
     },
 
+    // Parse URL path parameters
     parsePathParameters: function(request)
     {
         // If there's only one parameter, just treat it as a path
@@ -88,16 +97,38 @@ var common = {
         // Only parse input if it looks like it contains tags
         if (input.match(/<[^>]*>/))
         {
-            var doc = XmlService.parse(input);
-            return doc.getRootElement().getText();
+            // Find where the tags start & end
+            var start = input.indexOf('<');
+            var end = input.lastIndexOf('>') + 1;
+
+            // Grab any text before all XML tags
+            var pre = input.slice(0, start);
+            // Grab any text after all XML tags
+            var post = input.slice(end);
+            var inside = "";
+
+            try
+            {
+                // Parse input without any pre or post text
+                var cleanInput = input.slice(start, end);
+
+                var doc = XmlService.parse(cleanInput);
+                inside = doc.getRootElement().getText();
+            }
+            catch (error)
+            {
+                Logger.log(input + " = " + error);
+            }
+
+            return pre + inside + post;
         }
         return input;
     },
 
+    // Convert a JSON string to a pretty-print JSON string
     prettyPrintJsonStr: function(input)
     {
         return JSON.stringify(JSON.parse(input), null, 4);
     },
-    
-    ////////////////////////////////////////////////////
-}
+
+};
