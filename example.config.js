8c7f0f5eacce4ad5e10fb75c9b9e538771d1201f
Nitemice <webmaster@nitemice.net>
2021-10-16 22:02:46 +1100

    Added option to remove missing decklist files
    The script now keeps track of decklists.
    If a decklist that was previously created is not returned by the API,
    or is not in the expected folder, it will be deleted, if the option is
    selected.

--- example.config.js
+++ example.config.js
@@ -3,5 +3,6 @@ const config = {
     "userId": "1",
     "outputFormat": ["archidekt", "basic"],
 
-    "backupDir": "xzshxhX8QjQnlNuF_KWzSytboL6EvtSLW"
+    "backupDir": "xzshxhX8QjQnlNuF_KWzSytboL6EvtSLW",
+    "removeMissingDecklists": false
 };
